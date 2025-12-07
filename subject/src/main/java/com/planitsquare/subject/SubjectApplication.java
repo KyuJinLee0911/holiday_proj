814e0314576fe5a88328d2400e3a0f156b61ab15
rbwls1001 <rbwls1001@naver.com>
2025-12-07 12:08:51 +0900

    Feat: 매년 1월 2일 01:00 KST에 전년도, 금년도 데이터 자동 동기화

--- subject/src/main/java/com/planitsquare/subject/SubjectApplication.java
+++ subject/src/main/java/com/planitsquare/subject/SubjectApplication.java
@@ -2,12 +2,14 @@ package com.planitsquare.subject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SubjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SubjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SubjectApplication.class, args);
+    }
 
 }
--- subject/src/main/java/com/planitsquare/subject/domain/country/service/CountryReader.java
+++ subject/src/main/java/com/planitsquare/subject/domain/country/service/CountryReader.java
@@ -6,13 +6,18 @@ import com.planitsquare.subject.domain.country.repository.CountryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CountryReader {
     private final CountryRepository countryRepository;
 
     public Country getCountry(String countryCode) {
-        Country country = countryRepository.findByCountryCode(countryCode).orElseThrow(CountryNotFoundException::new);
-        return country;
+        return countryRepository.findByCountryCode(countryCode).orElseThrow(CountryNotFoundException::new);
+    }
+
+    public List<Country> getAll() {
+        return countryRepository.findAll();
     }
 }
